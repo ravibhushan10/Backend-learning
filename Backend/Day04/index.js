1912ef15747c72a10a2d76db40db8c6ea078fd65
Ravi Bhushan <ravibhushan87tp@gmail.com>
2025-10-27 03:37:25 +0530

    new

--- Backend/Day04/index.js
+++ Backend/Day04/index.js
@@ -1,18 +1,21 @@
 const http = require('http');
 
-
+// req=jo client data mangta hai
+// res= jo server server karega 
 const server = http.createServer((req,res)=>{
     
     // res.end("Hello Coder Army");
 
+
+    // routing : "/", "/contact", "/about"
     if(req.url==="/"){
-        res.end("Hello Coder Army");
+        res.end("Hello Coder Army how are you bro");   //send homepage(html,css,js file)
     }
     else if(req.url==="/contact"){
-        res.end("This is our Contact Page")
+        res.end("This is our Contact Page")   //send contact(html,css,js file)
     }
     else if(req.url==="/about"){
-        res.end("This is our About Page")
+        res.end("This is our About Page")    //send about (html,css,js file)
     }
     else
     {
@@ -21,6 +24,6 @@ const server = http.createServer((req,res)=>{
 });
 
 
-server.listen(4000, ()=>{
+server.listen(5000, ()=>{
     console.log("I am Listening at port number 4000");
 })
---
+++ Backend/Day04/notes.md
@@ -0,0 +1,12 @@
+To create the server you have to use the module "http"
+
+generally 0-1023 port are already reserved by https/http/ftp ....
+
+so use only above 1023 port number
+<!-- ****************************************** -->
+localhost:   4000
+khud ka ip   server port 
+<!-- ****************************************** -->
+abhi to plain text send(response) kar raha .. but aapko just html,css,js ki file send karni hai plain text ki jagah par.
+
+ 
